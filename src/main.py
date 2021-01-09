01afab1096e041663b977387706ad287dce8e788
Cooper Lockrey <usbhub95@gmail.com>
2021-01-09 12:28:04 +0000

    parade

--- src/main.py
+++ src/main.py
@@ -1,18 +1,40 @@
 import json
+import random
 
 #load save data from json
-with open('save.json','r') as infile:
-	save=json.load(infile)
+with open('data.json','r') as infile:
+	data=json.load(infile)
 
 
 
-print(save)
-save["player"]["food"]=2
-print(save)
+print(data)
+data["player"]["food"]=2
+print(data)
 
 
+def eventOutcome(eventName):
 
+  eventSet=[]
+  for i in data["random"][eventName]:
+    for o in range(i):
+      eventSet.append(data["random"][eventName][i])
+  
+  chosenEvent=eventSet[random.randint(0, len(eventSet))]
+	return chosenEvent
+
+chosenEvent=eventOutcome("exploration")
+
+if chosenEvent=="parade":
+  import parade
+elif 
+
+print(random("exploration"))
+
+#can easily have a specific thingo in the dict and then add one to the specific monster
+#for example each time they kill it
+#data["kills"]["chosenEvent or whatever"]+=1
+#get score value then divide it by lives and the final score can be a score per life
 
 #write save data to json
-with open('save.json','w') as outfile:
-	json.dump(save, outfile, indent=4)
+with open('data.json','w') as outfile:
+	json.dump(data, outfile, indent=4)
